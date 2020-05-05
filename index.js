3f1009cea667966f12c3278384ceedc3f5fdcd02
Mohit Singh <mohitsingh.shiv@gmail.com>
2020-05-05 15:17:14 +0530

    dev

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var express = require("express");
 var port = process.env.PORT || 3000;
 var app = express();
 app.post("/", function (req, res) {
-  res.send(res);
+  console.log(
+    "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+    res
+  );
+  res.send("res");
 });
 app.listen(port, function () {
   console.log(`Example app listening on port !`);
