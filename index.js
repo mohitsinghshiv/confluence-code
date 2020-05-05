fcecf38759c831ce652c8f3f999c4c025f62dc26
Mohit Singh <mohitsingh.shiv@gmail.com>
2020-05-05 15:03:07 +0530

    demo

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var port = process.env.PORT || 3000;
 var app = express();
-app.post("/", function (req, res) {
-  const body = JSON.parse(req.body);
-  res.send(JSON.stringify({ Hello: "World !!!!!!........" }));
+app.post("/", function (event) {
+  const body = JSON.parse(event.body);
+  res.send(JSON.stringify({ Hello: body }));
 });
 app.listen(port, function () {
   console.log(`Example app listening on port !`);
