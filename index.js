de0c43f1d627a95dfb7b2851b808c05c7af6aad1
Mohit Singh <mohitsingh.shiv@gmail.com>
2020-05-05 15:12:26 +0530

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var port = process.env.PORT || 3000;
 var app = express();
 app.post("/", function (req, res) {
-  res.send(JSON.stringify({ Hello: req }));
+  res.send("llllllllllllllllllll");
 });
 app.listen(port, function () {
   console.log(`Example app listening on port !`);
